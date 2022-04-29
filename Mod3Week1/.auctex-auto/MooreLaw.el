9cb834d5900d2b70d30c373ef050328fd64ae469
BuddhiLW <pedrogbranquinho@gmail.com>
2022-04-29 15:55:29 -0300

    Course 3, Week 1

---
+++ Mod3Week1/.auctex-auto/MooreLaw.el
@@ -0,0 +1,39 @@
+(TeX-add-style-hook
+ "MooreLaw"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:summary-activity"
+    "sec:org97939ab"
+    "sec:org5471b37"
+    "sec:org426a78d"
+    "sec:orga59ee5d"
+    "sec:org0a389d7"))
+ :latex)
+
